e2308207867f3da3446ddaf515151230c97ff982
Soumeya <ks_mekki@esi.dz>
2024-03-17 14:28:11 +0100

    backend init

---
+++ backend/src/main/java/com/example/TDM/TdmApplication.java
@@ -0,0 +1,13 @@
+package com.example.TDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdmApplication.class, args);
+	}
+
+}
