9b31e833c8bd3864e753e9583c403a5a3c3ce7d0
Soumeya <ks_mekki@esi.dz>
2024-03-17 14:24:21 +0100

    backend init

---
+++ backend/src/main/kotlin/com/example/TDM/TdmApplication.kt
@@ -0,0 +1,11 @@
+package com.example.TDM
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TdmApplication
+
+fun main(args: Array<String>) {
+	runApplication<TdmApplication>(*args)
+}
