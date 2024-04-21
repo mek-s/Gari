c5610983585072f41130f9f890c43f5217bac4b6
Soumeya <ks_mekki@esi.dz>
2024-04-21 14:57:41 +0100

    commiting when fasting lol

--- frontend/app/src/main/java/com/example/tdm/MainActivity.kt
+++ frontend/app/src/main/java/com/example/tdm/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ComponentActivity
 import com.example.tdm.ui.theme.TDMTheme
 
 class MainActivity : ComponentActivity() {
---
+++ frontend/app/src/main/java/com/example/tdm/data/models/Parking.kt
@@ -0,0 +1,6 @@
+package com.example.tdm.data.models
+
+
+data class Parking(
+
+)
