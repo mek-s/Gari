846a86e521ef822dd8272643fb5e01176721d8c9
Soumeya <ks_mekki@esi.dz>
2024-06-10 23:31:21 +0100

    search bar

--- frontend/app/src/main/java/com/example/tdm/ui/components/SearchBar.kt
+++ frontend/app/src/main/java/com/example/tdm/ui/components/SearchBar.kt
@@ -1,6 +1,7 @@
 package com.example.tdm.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,9 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -23,6 +31,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.tdm.data.models.Parking
 import androidx.compose.runtime.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SearchBar(
@@ -30,26 +42,60 @@ fun SearchBar(
     onQueryChanged: (String) -> Unit,
     onSearch: () -> Unit
 ) {
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(8.dp))
-            .padding(horizontal = 8.dp, vertical = 5.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(horizontal = 20.dp)
+            .clip(RoundedCornerShape(6.dp))
+            .background(Color.White)
     ) {
-        TextField(
-            value = query,
-            onValueChange = onQueryChanged,
-            modifier = Modifier.weight(1f),
-            placeholder = {
-                Text(text = "Search for parkings...")
-            }
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Button(onClick = onSearch) {
-            Text(text = "Search")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon",
+                tint = Color.Gray
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            BasicTextField(
+                value = query,
+                onValueChange = onQueryChanged,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions(onSearch = {
+                    // Handle the search action here
+                }),
+                singleLine = true,
+                textStyle = LocalTextStyle.current.copy(fontSize = 16.sp)
+            )
         }
     }
+//    Row(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(8.dp))
+//            .padding(horizontal = 8.dp, vertical = 5.dp),
+//        verticalAlignment = Alignment.CenterVertically
+//    ) {
+//        TextField(
+//            value = query,
+//            onValueChange = onQueryChanged,
+//            modifier = Modifier.weight(1f),
+//            placeholder = {
+//                Text(text = "Search for parkings...")
+//            }
+//        )
+//        Spacer(modifier = Modifier.width(8.dp))
+//        Button(onClick = onSearch) {
+//            Text(text = "Search")
+//        }
+//    }
 }
 
 @Composable
